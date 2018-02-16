935de5d5b8375783888b3108f460de457e7cda72
Theodore hughes <theodorehughes@Theodores-MacBook-Pro.local>
2018-02-15 23:19:19 -0600

    random word selection working

--- index.js
+++ index.js
@@ -26,12 +26,29 @@ var game = {
 			message: "Want to play hangman?"
 		}]).then(function(answer) {
 			if(answer.play){
-				currentGame.newGame();
+				console.log("Okay let's play!")
+				currentGame.new();
 			} else {
 				console.log("You decided not to play. Have a nice day!");
 			}
 		})
 	},//end of start function
+
+	new: function() {
+		console.log("starting new game...")
+		if (this.guessesLeft === 6) {
+			console.log("============================");
+			console.log("============================");
+
+			//creates a random number that will be used to select a word from the word bank array
+			var randNum = Math.floor(Math.random()*this.words.length);
+			console.log("random number is: " + randNum);
+		} else {
+			console.log("setting guesses left to 6...")
+			this.guessesLeft = 6;
+			this.new();
+		}
+	}
 }//end of game
 
 game.start();
